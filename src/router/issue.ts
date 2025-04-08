4a90883baf2bc77cef22835877c54bc316624bf9
niloyahmedrasel <niloyahmedrasel24@gmail.com>
2025-04-08 16:13:44 +0600

    changes in permission

--- src/router/issue.ts
+++ src/router/issue.ts
@@ -7,7 +7,7 @@ import { authorizeRoles } from "../middleware/authorizeUserRole";
 router.post("/",authenticateUser,authorizeRoles(["annotator","Super Admin"]), new IssueController().create);
 router.get("/",authenticateUser, new IssueController().getAllIssues);
 router.get("/:issueId",authenticateUser, new IssueController().getIssueById);
-router.put("/:issueId",authenticateUser,authorizeRoles(["annotator"]), new IssueController().update);
-router.delete("/:issueId",authenticateUser,authorizeRoles(["annotator"]), new IssueController().delete);
+router.put("/:issueId",authenticateUser,authorizeRoles(["Super Admin"]), new IssueController().update);
+router.delete("/:issueId",authenticateUser,authorizeRoles(["Super Admin"]), new IssueController().delete);
 
 export default router;
